165405198d5c13cd0434c222d8db14a474f6ce36
Osazuwa Matthew Ogbebor <osazuwamatthewogbebor@gmail.com>
2025-11-07 09:44:56 +0100

    handled pino pretty

--- src/config/logger.js
+++ src/config/logger.js
@@ -5,6 +5,7 @@ import APP_CONFIG from './APP_CONFIG.js';
 
 const __dirname = import.meta.dirname;
 const logPath = path.join(__dirname, '../..', 'logs', 'app.log');
+const isProd = APP_CONFIG.NODE_ENV === "production";
 
 const transport = pino.transport({
     targets: [{
@@ -21,7 +22,8 @@ const transport = pino.transport({
             colorize: true,
         },
         
-    }, {
+    }, isProd
+    ? {}:{
         level: APP_CONFIG.PINO_LOG_LEVEL_CONSOLE || 'info',
         target: 'pino-pretty',
         options: { colorize: true },
