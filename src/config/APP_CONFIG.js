2b04504a3ca918a5e3faf6825c4e6055a74a83e1
Osazuwa Matthew Ogbebor <osazuwamatthewogbebor@gmail.com>
2025-11-07 09:03:34 +0100

    added render postgres db

--- src/config/APP_CONFIG.js
+++ src/config/APP_CONFIG.js
@@ -8,11 +8,12 @@ dotenv.config();
     PORT: Number(process.env.PORT || 5000),
 
     DB_NAME: process.env.DB_NAME,
-    DB_HOST: process.env.DB_HOST,
+    DB_HOST: process.env.DB_HOST || "localhost",
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_PORT: process.env.DB_PORT,
     DATABASE_DIALECT:process.env.DATABASE_DIALECT || "mysql",
+    DATABASE_URL: process.env.DATABASE_URL,
 
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRES_IN: Number(process.env.JWT_EXPIRES_IN || 25200),
--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,19 +1,38 @@
 import { Sequelize } from 'sequelize';
 import APP_CONFIG from './APP_CONFIG.js';
+import dotenv from "dotenv";
+dotenv.config();
 
-//  Create Sequelize instance
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  // Use Render (production) database
+  sequelize = new Sequelize(APP_CONFIG.DATABASE_URL, {
+    dialect: "postgres", // or 'mysql'
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
+} else {
+  // Use local development database
+  sequelize = new Sequelize(
   APP_CONFIG.DB_NAME,
   APP_CONFIG.DB_USER,
   APP_CONFIG.DB_PASSWORD,
   {
     host: APP_CONFIG.DB_HOST,
     // dialect: 'mysql',
-    dialect: 'postgres',
+    dialect:  APP_CONFIG.DATABASE_DIALECT,
     logging: true,
     port: APP_CONFIG.DB_PORT
   }
 );
+
+}
 
 
 //  Test the connection immediately
